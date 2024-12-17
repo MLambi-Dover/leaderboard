a10a738ebaba70fccc6cf96fe9c01f13b9cd2547
Tyrus Berggren <tyrusbrianberggren@gmail.com>
2024-12-16 21:35:48 -0500

    Revert "styles beofore vertical responsiveness"
    This reverts commit 6142f9732a9952c5e2d674cf5ecbb5bc4afc98a1.

--- index.php
+++ index.php
@@ -2,86 +2,54 @@
 <html> 
 	<head> 
 		<title>LeaderBoard Using PHP</title> 
-		<link rel="stylesheet" href="./reset.css">
-		<link rel="stylesheet" href="./style.css">
-
-		<link rel="preconnect" href="https://fonts.googleapis.com">
-		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-		<link href="https://fonts.googleapis.com/css2?family=Berkshire+Swash&family=Josefin+Sans:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet">
-		<link rel="preconnect" href="https://fonts.googleapis.com">
-		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-		<link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet">
-
-		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<style>
+		.container {
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+			align-content: center;
+	</style>
 	</head> 
 
 	<body> 
 <div class="container">
-		<h2>Holiday Light Leaderboard</h2> 
+		<h2>Leaderboard</h2> 
 		<table> 
-			<thead>
+			<tr> 
+				<td>Ranking</td> 
+				<td>UserName</td> 
+				<td>Marks</td> 
+			</tr> 
+<?php 
 
-				<tr> 
-					<th>Ranking</th> 
-					<th>Name</th> 
-					<th>Marks</th> 
-				</tr> 
-			</thead>
-			<tbody>
-				<tr id="first">
-					<td>1</td>
-					<td>Peter Parker</td>
-					<td>500</td>
-				</tr>
-				<tr id="second">
-					<td>1</td>
-					<td>Peter Parker</td>
-					<td>500</td>
-				</tr>
-				<tr id="third">
-					<td>1</td>
-					<td>Peter Parker</td>
-					<td>500</td>
-				</tr>
-				<tr>
-					<td>1</td>
-					<td>Peter Parker</td>
-					<td>500</td>
-				</tr>
-				<tr>
-					<td>1</td>
-					<td>Peter Parker</td>
-					<td>500</td>
-				</tr>
-				<tr>
-					<td>1</td>
-					<td>Peter Parker</td>
-					<td>500</td>
-				</tr>
-				<tr>
-					<td>1</td>
-					<td>Peter Parker</td>
-					<td>500</td>
-				</tr>
-				<tr>
-					<td>1</td>
-					<td>Peter Parker</td>
-					<td>500</td>
-				</tr>
-				<tr>
-					<td>1</td>
-					<td>Peter Parker</td>
-					<td>500</td>
-				</tr>
-				<tr>
-					<td>1</td>
-					<td>Peter Parker</td>
-					<td>500</td>
-				</tr>
-			</tbody>
-			</table> 
-		</div>
+/* Connection Variable ("Servername", 
+"username","password","database") */
+$con = mysqli_connect("localhost", 
+		"hrlvesmy_leaderboard",
+		"O6,&*7Y(oCy{",
+		"hrlvesmy_leaderboard"); 
+
+/* Mysqli query to fetch rows 
+in descending order of marks */
+$result = mysqli_query($con, "SELECT userName, 
+marks FROM leaderboard ORDER BY marks DESC LIMIT 10"); 
+
+/* First rank will be 1 and 
+	second be 2 and so on */
+$ranking = 1; 
+
+/* Fetch Rows from the SQL query */
+if (mysqli_num_rows($result)) { 
+	while ($row = mysqli_fetch_array($result)) { 
+		echo "<tr> <td>{$ranking}</td> 
+		<td>{$row['userName']}</td> 
+		<td>{$row['marks']}</td></tr>"; 
+		$ranking++; 
+	} 
+} 
+?> 
+</table> 
+</div>
 </body> 
 </html> 
 
