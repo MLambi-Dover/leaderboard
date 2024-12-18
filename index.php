c49567349d91ecff6cd8cfa550671fb471f54126
MLambi-Dover <101574810+MLambi-Dover@users.noreply.github.com>
2024-12-18 09:54:07 -0500

    Update index.php

--- index.php
+++ index.php
@@ -51,11 +51,22 @@ $ranking = 1;
 /* Fetch Rows from the SQL query */
 if (mysqli_num_rows($result)) { 
 	while ($row = mysqli_fetch_array($result)) { 
-			if ($rankiing == 1) {
+			if ($ranking == 1) {
 				echo "<tr id='first'> <td>{$ranking}</td> 
 				<td>{$row['userName']}</td> 
 				<td>{$row['marks']}</td></tr>"; 
 				$ranking++;
+			} elseif ($ranking == 2){
+				echo "<tr id='second'> <td>{$ranking}</td> 
+				<td>{$row['userName']}</td> 
+				<td>{$row['marks']}</td></tr>"; 
+				$ranking++;
+			} elseif {
+				echo "<tr id='third'> <td>{$ranking}</td> 
+				<td>{$row['userName']}</td> 
+				<td>{$row['marks']}</td></tr>"; 
+				$ranking++;
+			
 			} else {
 		
 			echo "<tr> <td>{$ranking}</td> 
