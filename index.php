d71f548b8247688067839186656662e9a3cde315
MLambi-Dover <101574810+MLambi-Dover@users.noreply.github.com>
2024-12-18 09:56:31 -0500

    Update index.php

--- index.php
+++ index.php
@@ -56,23 +56,11 @@ if (mysqli_num_rows($result)) {
 				<td>{$row['userName']}</td> 
 				<td>{$row['marks']}</td></tr>"; 
 				$ranking++;
-			} elseif ($ranking == 2){
-				echo "<tr id='second'> <td>{$ranking}</td> 
-				<td>{$row['userName']}</td> 
-				<td>{$row['marks']}</td></tr>"; 
-				$ranking++;
-			} elseif {
-				echo "<tr id='third'> <td>{$ranking}</td> 
-				<td>{$row['userName']}</td> 
-				<td>{$row['marks']}</td></tr>"; 
-				$ranking++;
-			
 			} else {
-		
-			echo "<tr> <td>{$ranking}</td> 
-			<td>{$row['userName']}</td> 
-			<td>{$row['marks']}</td></tr>"; 
-			$ranking++; 
+				echo "<tr> <td>{$ranking}</td> 
+				<td>{$row['userName']}</td> 
+				<td>{$row['marks']}</td></tr>"; 
+				$ranking++; 
 			}
 	} 
 } 
